fae0d28b8a78db4c7f9baf6dd29535c99bd0fb4a
sriram230107 <rsbb7703@gmail.com>
2026-08-18 21:37:29 +0530

    error fix

--- frontend/src/pages/TeamLeadDashboard.tsx
+++ frontend/src/pages/TeamLeadDashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import api from '../api/axios';
 import { PaginatedTable } from '../components/PaginatedTable';
 import { StatusBadge } from '../components/StatusBadge';
 
@@ -10,7 +10,7 @@ export const TeamLeadDashboard: React.FC = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const res = await axios.get('http://localhost:3000/api/v1/dashboard/team-lead', { withCredentials: true });
+                const res = await api.get('/dashboard/team-lead');
                 setData(res.data);
             } catch (e) {
                 console.error(e);
